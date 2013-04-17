858faa2457c2314298fdbd2b737e0e2d95ef9704
Manuel Estebanez <manu3d@iname.com>
2013-04-17 17:51:13 +0200

    finish sign in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -11,7 +11,7 @@ describe "AuthenticationPages" do
       before { click_button "Sign in" }
 
       it { should have_selector('title', text: 'Sign in') }
-      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      it { should have_error_message('Invalid') }
 
       describe "after visiting another page" do
         before { click_link "Home" }
@@ -21,11 +21,7 @@ describe "AuthenticationPages" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { valid_signin(user) }
 
       describe "followed by signout" do
         before { click_link "Sign out" }
